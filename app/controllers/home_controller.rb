361e6dfaee5cb37d549da160a6ed3de453e4a316
Ngô Trung Hưng <hungnt@zigexn.vn>
2020-07-31 15:49:06 +0700

    fix toppage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,8 @@ class HomeController < ApplicationController
     @industries = Industry.order(name: :asc).all
     @job_count = Job.all.count
     @cities = City.all
-    @five_jobs = Job.order(created_at: :desc).limit(5)
-    @top_city = City.top_hot.take(9)
-    @top_industry = Industry.top_hot.take(9)
+    @lasted_jobs = Job.order(created_at: :desc).limit(Job::NUMBER_LASTED_JOB)
+    @top_cities = City.top_hot.take(9)
+    @top_industries = Industry.top_hot.take(9)
   end
 end
