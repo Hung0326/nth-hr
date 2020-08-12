8a4075568e36e143aed17e315ab232636ab5e75b
Ngô Trung Hưng <hungnt@zigexn.vn>
2020-08-12 11:30:52 +0700

    delete test

--- test/controllers/industry_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class IndustryControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get industry_index_url
-    assert_response :success
-  end
-
-end
